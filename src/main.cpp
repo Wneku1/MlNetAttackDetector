276bf5b762db1e45e2ae15176d5d67ce6925c7e2
Wneku <sebastianwnek712@gmail.com>
2022-04-25 22:44:24 +0200

    temporary doesnt work

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
+#include "mlpack/core.hpp"
+#include "mlpack/core/cv/k_fold_cv.hpp"
+#include "mlpack/core/cv/metrics/accuracy.hpp"
+#include "mlpack/core/cv/metrics/f1.hpp"
+#include "mlpack/core/cv/metrics/precision.hpp"
+#include "mlpack/core/cv/metrics/recall.hpp"
+#include "mlpack/methods/decision_tree/random_dimension_select.hpp"
+#include "mlpack/methods/random_forest/random_forest.hpp"
 #include <iostream>
-#include <mlpack/core.hpp>
-#include <string_view>
 
-void loadAndPrintData( std::string_view pathToDataset )
-{
-	using namespace mlpack;
-	arma::mat data;
-	data::Load( pathToDataset.data(), data, true );
-	data.print();
-}
+using namespace arma;
+using namespace mlpack;
+using namespace mlpack::tree;
+using namespace mlpack::cv;
 
 int main( int argc, char* argv[] )
 {
 	constexpr auto firstArgument{ 1 };
-	const auto pathToDataset{ argv[ firstArgument ] };
-	loadAndPrintData( pathToDataset );
+	mat dataset;
+	bool loaded = mlpack::data::Load( argv[ firstArgument ], dataset );
+	if( !loaded )
+	{
+		return -1;
+	}
+
+	// Row< size_t > labels;
+	// labels = conv_to< Row< size_t > >::from( dataset.row( dataset.n_rows - 1 )
+	// );
+	dataset.print();
+	// Row< size_t > labels = conv_to< Row< size_t > >::from( dataset.row(
+	// dataset.n_rows - 1 ) ); std::cout << labels.at( 0 ); dataset.print();
+	// dataset.shed_row( dataset.n_rows - 1 );
+	// dataset.save( "qwe.csv", auto_detect );
+	// dataset.shed_row( dataset.n_rows - 1 );
+
+	// const size_t numClasses		 = 2;
+	// const size_t minimumLeafSize = 5;
+	// const size_t numTrees		 = 10;
+	// RandomForest< GiniGain, RandomDimensionSelect > rf;
+	// rf = RandomForest< GiniGain, RandomDimensionSelect >( dataset, labels,
+	// numClasses, numTrees, minimumLeafSize );
+
+	// Row< size_t > predictions;
+	// rf.Classify( dataset, predictions );
+	// const size_t correct = arma::accu( predictions == labels );
+	// cout << "\nTraining Accuracy: " << ( double( correct ) / double(
+	// labels.n_elem ) ) << "\n";
+
 	return 0;
 }
