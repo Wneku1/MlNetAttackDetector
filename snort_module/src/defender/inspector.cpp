0e8797657b1fd5e7f4b89f3bff848c9eedf4bb74
Enkapsulation <kosciej.rafal@gmail.com>
2022-05-30 19:58:15 +0200

    .

---
+++ snort_module/src/defender/inspector.cpp
@@ -0,0 +1,65 @@
+#include "inspector.hpp"
+#include "config.hpp"
+#include "module.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+#include "log/messages.h"
+
+using namespace snort;
+using namespace Defender;
+
+//-------------------------------------------------------------------------
+// implementation stuff
+//-------------------------------------------------------------------------
+void InspectorConf::showConfig() const
+{
+  ConfigLogger::log_option("defender");
+  if (m_config) { ConfigLogger::log_list("", std::string(*m_config).c_str()); }
+}
+
+//-------------------------------------------------------------------------
+// class stuff
+//-------------------------------------------------------------------------
+InspectorConf::InspectorConf(const std::shared_ptr<Config> &config, ModuleConf *module)
+  : m_config(config), m_module(module)
+{}
+
+InspectorConf::~InspectorConf() = default;
+
+void InspectorConf::show(const SnortConfig *) const { showConfig(); }
+
+bool InspectorConf::validate(const Packet *packet) const { return packet->flow; }
+
+std::string InspectorConf::getClientIp(const Packet *packet) const
+{
+  char clientIp[INET6_ADDRSTRLEN];
+  packet->flow->client_ip.ntop(clientIp, sizeof(clientIp));
+
+  return clientIp;
+}
+
+std::string InspectorConf::getServerIp(const Packet *packet) const
+{
+  char serverIp[INET6_ADDRSTRLEN];
+  packet->flow->server_ip.ntop(serverIp, sizeof(serverIp));
+
+  return serverIp;
+}
+
+PegCount InspectorConf::getPacketsCount() const { return *m_module->get_counts(); }
+
+void InspectorConf::eval(Packet *packet)
+{
+  if (!m_initStaus) { m_initStaus = true; }
+
+  m_module->incrementPacketCounter();
+
+  if (!validate(packet)) { return; }
+
+  const auto clientIp{ getClientIp(packet) };
+}
--- snort_module/src/detector/parameters_name.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string_view>
-
-namespace Parameters::Name
-{
-
-} // namespace Parameters::Name
